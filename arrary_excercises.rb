0e9050ca699afb0ca9b859af9e7c4bead682021d
Nalipp <nateplipp@gmail.com>
2015-12-28 21:40:09 +0900

    arrays excercises

---
+++ arrary_excercises.rb
@@ -0,0 +1,26 @@
+=begin
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+puts arr.include?(number)
+=end
+
+=begin
+arr = [["test", "hello", "world"],["example", "mem"]]
+puts arr[1][0]
+=end
+
+=begin
+names = ['bob', 'joe', 'susan', 'margaret']
+names[3] = 'jody'
+p names
+=end
+
+=begin
+arr_one = [1, 2, 3, 4, 5]
+
+arr_two = arr_one.map { |i| i + 2 }
+
+p arr_one
+p arr_two
+=end
