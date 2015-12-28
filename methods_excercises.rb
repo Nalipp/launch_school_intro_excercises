5f8f93cf3054eff19045a311d269fcc216049978
Nalipp <nateplipp@gmail.com>
2015-12-28 13:27:59 +0900

    methods excercises

---
+++ methods_excercises.rb
@@ -0,0 +1,24 @@
+=begin
+def greeting(name)
+  puts 'Hello ' + name + '.'
+end
+
+greeting('nate')
+=end
+
+=begin
+def multiply(num1, num2)
+  num1 * num2
+end
+
+puts multiply(9, 4)
+=end
+
+=begin
+def scream(words)
+  words = words + "!!!!!"
+  puts words
+end
+
+scream("Yipeee")
+=end
---
+++ return.rb
@@ -0,0 +1,38 @@
+=begin
+def add_three(number)
+  return number + 3
+  number + 4
+end
+
+returned_value = add_three(4)
+puts returned_value
+=end
+
+=begin
+def add_three(n)
+  puts n + 3
+end
+
+add_three(5).times { puts 'this sholud print 8 times'}
+=end
+
+=begin
+def add(x, y)
+  x + y
+end
+
+puts add(34, 55)
+
+def subtract(x, y)
+  x - y
+end
+
+puts subtract(33, 22)
+
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+puts multiply(subtract(33, 22), add(34, 55))
+=end
