7ffe416188c931a2165c962fde789d64722da961
Nalipp <nateplipp@gmail.com>
2015-12-28 10:36:14 +0900

    finished excercises

--- the_basics.rb
+++ the_basics.rb
@@ -1,1 +1,43 @@
-puts 'hello'
+=begin
+name = 'nate ' + 'lipp'
+puts name
+=end
+
+=begin
+num = 9876
+puts ones = num.to_i % 10
+puts tens = num.to_i % 100 / 10
+puts hundreds = num.to_i % 1000 / 100
+puts thousands = num.to_i % 10000 / 1000
+=end
+
+=begin
+movies = {:jaws => '1980', :home_alone => '1990', :scream => '2000'}
+puts movies[:jaws]
+puts movies[:home_alone]
+puts movies[:scream]
+=end
+
+=begin
+array = []
+movies = {:jaws => '1980', :home_alone => '1990', :scream => '2000'}
+array << movies[:jaws]
+array << movies[:home_alone]
+array << movies[:scream]
+puts array[0]
+puts array[1]
+puts array[2]
+=end
+
+=begin
+puts factoral_5 = 5 * 4 * 3 * 2 * 1
+puts factoral_6 = 6 * 5 * 4 * 3 * 2 * 1
+puts factoral_7 = 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts factoral_8 = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+=end
+
+=begin
+puts 5.45 ** 2
+puts 4.99 ** 2
+puts 3.87 ** 2
+=end
